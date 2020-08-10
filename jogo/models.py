ccb88f8741c9889714f4b7bd3d1280a72f51e370
BrunoCAF <brunocaf123@gmail.com>
2020-08-10 17:10:17 -0300

    consertei o banco

--- jogo/models.py
+++ jogo/models.py
@@ -6,7 +6,7 @@ class ScoreEntry(models.Model):
     username = models.CharField(max_length=60)
     userid = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
     conclusionTime = models.FloatField(db_index=True)
-    ghostInfo = models.TextField(serialize=True, max_length=1023)
+    ghostInfo = models.TextField(serialize=True, max_length=1000000)
 
     def __str__(self):
         return self.username + ": " + str(self.conclusionTime)
