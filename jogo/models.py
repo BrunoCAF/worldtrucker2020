605ce5a59073c4a2c3727a80f0b4aa2aead98c3a
BrunoCAF <brunocaf123@gmail.com>
2020-08-10 17:01:17 -0300

    limpei o banco

--- jogo/models.py
+++ jogo/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class ScoreEntry(models.Model):
     username = models.CharField(max_length=60)
-    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    userid = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
     conclusionTime = models.FloatField(db_index=True)
     ghostInfo = models.TextField(serialize=True, max_length=1023)
 
